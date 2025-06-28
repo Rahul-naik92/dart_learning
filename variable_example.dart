9cf347eaf3a207d25e051568b8892276e964e7e6
Rahul-naik92 <team.rahulnaik@gmail.com>
2025-06-28 20:43:32 +0530

    usage of variable

---
+++ variable_example.dart
@@ -0,0 +1,34 @@
+//Declare variable in Dart.
+//What are build-in Data Types in Dart.
+  /* Numbers
+      int
+      double
+    Strings
+    Booleans
+    Lists - Also known as Arrays
+    Maps
+    Runes - For expressing Unicode character in a String
+    Symbols
+    Note: All data types in Dart are Objects
+        The default value will be always null
+    */
+
+void main(){
+
+//Numbers: int
+int score =23;
+
+//Numbers: double
+double percentage=93.4;
+
+//Strings
+String name="Herry";
+
+//Boolean
+bool isValid=true;
+
+print(score);
+print(percentage);
+print(name);
+print(isValid);
+}
