5228cd98bbba29759bbc700c0c8677b8062fe73d
Rahul-naik92 <team.rahulnaik@gmail.com>
2025-06-29 20:27:25 +0530

    usage of string

---
+++ sting_usage.dart
@@ -0,0 +1,28 @@
+//What are Literals
+//Various ways to write String Literals
+//String Interpolation
+
+
+
+void main()
+{
+    //Literals and how to define String Literals in Dart.
+
+    String s1='Single';
+    String s2='Double';
+    String s3='It\'s easy';
+    String s4= "Its easy";
+String s5='This is going to be very long String'
+            'This is again big String';
+
+            //String Interpolation
+
+            String name='Hevin';
+            print("My name is $name");
+            print("Length of String is ${name.length}");
+
+            int l=20;
+            int b=30;
+            print("The sum of $l and $b is ${l+b}");
+
+}
